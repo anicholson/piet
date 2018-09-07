c08eb42f0876879d64b20f08c9ea5f54712d690f
Andy Nicholson <andrew@anicholson.net>
2018-09-08 08:51:01 +1000

    Add Multiply command

---
+++ spec/piet_interpreter/commands/multiply_spec.cr
@@ -0,0 +1,26 @@
+require "../../spec_helper"
+
+module PietInterpreter
+  module Commands
+    describe Multiply do
+      it "pops the top two numbers, multiplies them, and places the result on the top of the stack" do
+        context = Context.new
+        context.stack = Stack.new([2_i64, 3_i64, 5_i64])
+
+        new_context  = Multiply.new.run(context)
+
+        new_context.stack.size.should eq 2
+        new_context.stack.first.should eq 15
+      end
+
+      it "ignores if the stack is too small" do
+        context = Context.new
+        context.stack = Stack.new([2_i64])
+
+        new_context = Multiply.new.run(context)
+
+        new_context.should eq context
+      end
+    end
+  end
+end
